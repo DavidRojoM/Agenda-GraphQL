f3985b875c52fbb45fba798f4b4e757471e8a0f7
David Rojo <davidrojo1997@gmail.com>
2021-11-28 15:53:40 +0100

    feat(contacts): add contacts service implementation

--- backend/src/contacts/contacts.service.ts
+++ backend/src/contacts/contacts.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ContactsRepository } from './repository/contacts-repository/contacts-repository.service';
+import { ContactDTO } from './dto/contactDTO';
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+  constructor(private contactRepository: ContactsRepository) {}
+
+  getContacts = () => this.contactRepository.findAll();
+
+  createContact = (contact: ContactDTO) =>
+    this.contactRepository.addOne(contact);
+
+  deleteContact = (id: string) => this.contactRepository.deleteOne(id);
+
+  updateContact = (options: { contact: ContactDTO; id: string }) =>
+    this.contactRepository.updateOne(options);
+}
