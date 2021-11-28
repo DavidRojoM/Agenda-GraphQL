7b74c08c1aa2424a12c7ad606fe25a857acee904
David Rojo <davidrojo1997@gmail.com>
2021-11-28 02:35:50 +0100

    feat(routing): add first routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/add/add.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  },
+  {
+    path: 'add/:id',
+    component: AddComponent,
+  },
+  {
+    path: 'update/:id',
+    component: UpdateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
