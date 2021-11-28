9d4a4ff5ad0ed1ab653aa017baf336bea64286a4
David Rojo <davidrojo1997@gmail.com>
2021-11-28 12:41:36 +0100

    build(environment): set port to 3000

--- backend/src/environment.ts
+++ backend/src/environment.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 export const environment = {
-  appPort: process.env.PORT || 4200,
+  appPort: process.env.PORT || 3000,
   dbPort: process.env.DB_PORT || 27017,
   dbHost: process.env.DB_HOST || 'localhost',
   dbUser: process.env.DB_USER || 'root',
