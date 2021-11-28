eaaa9d4782e31159e0a2b0a428454d4d95c043f8
David Rojo <davidrojo1997@gmail.com>
2021-11-28 15:47:34 +0100

    feat(app): add mongoose connection string

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ContactsModule } from './contacts/contacts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './environment';
 
 @Module({
-  imports: [ContactsModule],
+  imports: [
+    ContactsModule,
+    MongooseModule.forRoot(`mongodb://${environment.dbHost}`, {
+      dbName: environment.dbName,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
