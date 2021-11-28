0a5355178f780959ba7ccc2ff0657bf0597f5482
David Rojo <davidrojo1997@gmail.com>
2021-11-28 13:53:47 +0100

    feat(contacts): add mongoose connection string

--- backend/src/contacts/contacts.module.ts
+++ backend/src/contacts/contacts.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
 import { ContactsRepository } from './repository/contacts-repository/contacts-repository.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from '../environment';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${environment.dbUser}:${environment.dbPassword}@${environment.dbHost}:${environment.dbPort}/${environment.dbName}`,
+    ),
+  ],
   controllers: [ContactsController],
   providers: [ContactsService, ContactsRepository],
 })
