9c4130247fce82232061027d44f30b497f511e28
David Rojo <davidrojo1997@gmail.com>
2021-11-28 15:53:04 +0100

    feat(repository): add contacts repository implementation

--- backend/src/contacts/repository/contacts-repository/contacts-repository.service.ts
+++ backend/src/contacts/repository/contacts-repository/contacts-repository.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Contact } from '../../models/contact.interface';
+import { ContactDTO } from '../../dto/contactDTO';
 
 @Injectable()
-export class ContactsRepository {}
+export class ContactsRepository {
+  constructor(@InjectModel('Contact') private contactModel: Model<Contact>) {}
+
+  findAll = async (): Promise<ContactDTO[]> => this.contactModel.find();
+
+  addOne = async (contact: ContactDTO): Promise<ContactDTO> => {
+    const newContact = new this.contactModel(contact);
+    return newContact.save();
+  };
+
+  deleteOne = async (id: string) => this.contactModel.findByIdAndDelete(id);
+
+  updateOne = ({ id, contact }: { contact: ContactDTO; id: string }) =>
+    this.contactModel.findByIdAndUpdate(id, contact);
+}
