66837d235df37504c9e1e1e4b8907f2a31440bd5
David Rojo <davidrojo1997@gmail.com>
2021-11-29 16:56:46 +0100

    feat(shared): add contacts service

--- frontend/src/app/shared/services/contacts.service.ts
+++ frontend/src/app/shared/services/contacts.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { ContactsRepositoryService } from './repository/contacts-repository.service';
+import { Contact } from '../models/contact';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactsService {
+  constructor(private readonly contactsRepository: ContactsRepositoryService) {}
 
-  constructor() { }
+  findContacts = () => this.contactsRepository.findContacts();
+
+  addContact = (contact: Contact) =>
+    this.contactsRepository.addContact(contact);
+
+  updateContact = (contact: Contact) =>
+    this.contactsRepository.updateContact(contact);
+
+  deleteContact = (contact: Contact) => {
+    if (!contact.id) {
+      return new Error('Contact id not found');
+    }
+    return this.contactsRepository.deleteContact(contact.id);
+  };
 }
