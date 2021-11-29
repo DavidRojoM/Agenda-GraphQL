e463c0e0aeecb2936f9fc1e76c07e1671b49aea8
David Rojo <davidrojo1997@gmail.com>
2021-11-29 23:53:24 +0100

    feat(actions): add actions

--- frontend/src/app/state/actions/contacts.actions.ts
+++ frontend/src/app/state/actions/contacts.actions.ts
@@ -2,31 +2,59 @@ import { ActionTypes } from './action.types.enum';
 import { createAction, props } from '@ngrx/store';
 import { Contact } from '../../shared/models/contact';
 
+//TODO: FIX UPDATE
 export const actions = {
   loadContactsRequest: createAction(ActionTypes.LOAD_CONTACTS_REQUEST),
 
   loadContactsSuccess: createAction(
     ActionTypes.LOAD_CONTACTS_SUCCESS,
-    props<{ contacts: Contact[]; loading: false }>()
+    props<{ contacts: Contact[] }>()
   ),
 
   loadContactsFailure: createAction(
     ActionTypes.LOAD_CONTACTS_FAILURE,
-    props<{ error: string; loading: false }>()
+    props<{ error: string }>()
   ),
 
   addContactRequest: createAction(
     ActionTypes.ADD_CONTACT_REQUEST,
-    props<{ contact: Contact; loading: true }>()
+    props<{ contact: Contact }>()
   ),
 
   addContactSuccess: createAction(
     ActionTypes.ADD_CONTACT_SUCCESS,
-    props<{ contact: Contact; loading: false }>()
+    props<{ contact: Contact }>()
   ),
 
   addContactFailure: createAction(
     ActionTypes.ADD_CONTACT_FAILURE,
-    props<{ error: string; loading: false }>()
+    props<{ error: string }>()
+  ),
+  updateContactRequest: createAction(
+    ActionTypes.UPDATE_CONTACTS_REQUEST,
+    props<{ contact: Contact }>()
+  ),
+
+  updateContactSuccess: createAction(
+    ActionTypes.UPDATE_CONTACT_SUCCESS,
+    props<{ contact: Contact }>()
+  ),
+
+  updateContactFailure: createAction(
+    ActionTypes.UPDATE_CONTACT_FAILURE,
+    props<{ error: string }>()
+  ),
+
+  removeContactRequest: createAction(
+    ActionTypes.REMOVE_CONTACT_REQUEST,
+    props<{ id: string }>()
+  ),
+  removeContactSuccess: createAction(
+    ActionTypes.REMOVE_CONTACT_SUCCESS,
+    props<{ id: string }>()
+  ),
+  removeContactFailure: createAction(
+    ActionTypes.REMOVE_CONTACT_FAILURE,
+    props<{ error: string }>()
   ),
 };
