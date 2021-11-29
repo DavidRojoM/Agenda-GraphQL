224fe8d5122a7ba8e34961b33d2ff6b5532de125
David Rojo <davidrojo1997@gmail.com>
2021-11-29 14:35:04 +0100

    feat(shared): add contact repository and first methods

--- frontend/src/app/shared/services/repository/contacts-repository.service.ts
+++ frontend/src/app/shared/services/repository/contacts-repository.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactDTO } from '../../dto/contact-dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactsRepositoryService {
+  BASE_URL = 'http://localhost:3000/api/v1/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  findContacts = () => this.http.get(`${this.BASE_URL}/contats`);
+
+  addContact = (contact: ContactDTO) =>
+    this.http.post(`${this.BASE_URL}/contacts`, contact);
+
+  updateContact = (contact: ContactDTO) =>
+    this.http.put(`${this.BASE_URL}/contacts/${contact.id}`, contact);
+
+  deleteContact = (contactId: string) =>
+    this.http.delete(`${this.BASE_URL}/contacts/${contactId}`);
 }
