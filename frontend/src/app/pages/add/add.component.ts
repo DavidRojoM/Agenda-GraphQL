d535daebc09480166fbe158e88af7778994f91af
David Rojo <davidrojo1997@gmail.com>
2021-11-29 23:54:08 +0100

    feat(add component): add callback

--- frontend/src/app/pages/add/add.component.ts
+++ frontend/src/app/pages/add/add.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { actions } from '../../state/actions/contacts.actions';
+import { Contact } from '../../shared/models/contact';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  constructor(private store: Store<any>) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  addAction = (contact: Contact) => {
+    this.store.dispatch(actions.addContactRequest({ contact }));
+  };
 }
