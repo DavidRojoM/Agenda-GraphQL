f5340820e564e0310b2fa76f66499e6abd99824e
David Rojo <davidrojo1997@gmail.com>
2021-11-29 17:13:49 +0100

    feat(actions): add ActionTypes enum

---
+++ frontend/src/app/state/actions/action.types.enum.ts
@@ -0,0 +1,14 @@
+export enum ActionTypes {
+  LOAD_CONTACTS_REQUEST = '[Contact] Load Contacts Request',
+  LOAD_CONTACTS_FAILURE = '[Contact] Load Contacts Failure',
+  LOAD_CONTACTS_SUCCESS = '[Contact] Load Contacts Failure',
+  ADD_CONTACT_REQUEST = '[Contact] Add Contact Request',
+  ADD_CONTACT_FAILURE = '[Contact] Add Contact Failure',
+  ADD_CONTACT_SUCCESS = '[Contact] Add Contact Success',
+  UPDATE_CONTACTS_REQUEST = '[Contact] Update Contact Request',
+  UPDATE_CONTACT_FAILURE = '[Contact] Update Contact Failure',
+  UPDATE_CONTACT_SUCCESS = '[Contact] Update Contact Success',
+  REMOVE_CONTACT_REQUEST = '[Contact] Remove Contact Request',
+  REMOVE_CONTACT_FAILURE = '[Contact] Remove Contact Failure',
+  REMOVE_CONTACT_SUCCESS = '[Contact] Remove Contact Success',
+}
