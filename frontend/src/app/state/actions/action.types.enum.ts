601f7e7d8e3af514c7e0b5238785c8deb9210997
David Rojo <davidrojo1997@gmail.com>
2021-11-29 19:20:26 +0100

    fix(state): fix ActionTypes typo that MADE ME LOSE 4HOURS

--- frontend/src/app/state/actions/action.types.enum.ts
+++ frontend/src/app/state/actions/action.types.enum.ts
@@ -1,7 +1,7 @@
 export enum ActionTypes {
   LOAD_CONTACTS_REQUEST = '[Contact] Load Contacts Request',
   LOAD_CONTACTS_FAILURE = '[Contact] Load Contacts Failure',
-  LOAD_CONTACTS_SUCCESS = '[Contact] Load Contacts Failure',
+  LOAD_CONTACTS_SUCCESS = '[Contact] Load Contacts Success',
   ADD_CONTACT_REQUEST = '[Contact] Add Contact Request',
   ADD_CONTACT_FAILURE = '[Contact] Add Contact Failure',
   ADD_CONTACT_SUCCESS = '[Contact] Add Contact Success',
