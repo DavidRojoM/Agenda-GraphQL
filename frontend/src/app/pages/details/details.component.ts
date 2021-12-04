7013dab9838deaa0838e9af7fbca362675b29a30
David Rojo <davidrojo1997@gmail.com>
2021-12-04 22:48:26 +0100

    feat(details component): add loader spinner on refresh

--- frontend/src/app/pages/details/details.component.ts
+++ frontend/src/app/pages/details/details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectContactsLoading } from '../../state/selectors/contacts.selector';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  loading$!: Observable<boolean>;
 
-  constructor() { }
+  constructor(private store: Store<any>) {}
 
   ngOnInit(): void {
+    this.loading$ = this.store.select(selectContactsLoading);
   }
-
 }
