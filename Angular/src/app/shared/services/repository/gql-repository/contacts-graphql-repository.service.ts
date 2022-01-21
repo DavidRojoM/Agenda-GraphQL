4b9e5ed93a7736bd6542cde0c1cae5bb4e6feee6
David Rojo <davidrojo1997@gmail.com>
2022-01-21 13:01:14 +0100

    feat(graphql-repository): add delete impl

--- Angular/src/app/shared/services/repository/gql-repository/contacts-graphql-repository.service.ts
+++ Angular/src/app/shared/services/repository/gql-repository/contacts-graphql-repository.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Apollo, gql } from 'apollo-angular';
-import { RepositoryInterface } from '../../../common/repository.interface';
-import { Observable, of } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { RepositoryInterface } from '../../../../common/repository.interface';
+import { Observable } from 'rxjs';
+import { getContactsQuery } from './queries/get-contacts.query';
+import { map } from 'rxjs/operators';
+import { addContactMutation } from './mutations/add-contact.mutation';
+import { Contact } from '../../../domain/models/contact';
+import { updateContactMutation } from './mutations/update-contact.mutation';
+import { deleteContactMutation } from './mutations/delete-contact.mutation';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +15,22 @@ import { Observable, of } from 'rxjs';
 export class ContactsGraphqlRepositoryService implements RepositoryInterface {
   constructor(private apollo: Apollo) {}
 
-  delete<T>(url: string): Observable<T> {
-    return of<T>();
+  delete<T>(id: string): Observable<T> {
+    console.log(id);
+    return this.apollo
+      .mutate<T>({
+        mutation: deleteContactMutation,
+        variables: {
+          deleteContactData: {
+            id: id,
+          },
+        },
+      })
+      .pipe(
+        map(({ data }: any) => {
+          return data.deleteContact;
+        })
+      );
   }
 
   get<T>(url: string): Observable<T> {
